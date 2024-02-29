1ec5a468731e7bc5bcf635cba9a2f0e8baec650b
ᴹᴿDΔRҜ LUSIҒΞR <cybershareofficial@gmail.com>
2024-02-29 19:22:12 +0530

    Update chatGPT.py

--- chatGPT.py
+++ chatGPT.py
@@ -1,11 +1,17 @@
 # ===================Demo code==================== #
 import requests
 
+#GET method
+url = "https://testapi-pwp4.onrender.com/api/ai?prompt=YOUR_TEXT"
+re = get(url)
+
+print(re.text)
+
+#POST method
 url = "https://testapi-pwp4.onrender.com/api/ai"
 data = {
-    "prompt":"hello"
+    "prompt":"YOUR_TEXT"
     }
-
 re = requests.post(url, json=data)
 
 print(re.text)
